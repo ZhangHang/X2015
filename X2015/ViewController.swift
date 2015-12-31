56731c024f6960203cad12cd72999d608b1ce044
ZhangHang <zh199111@gmail.com>
2015-12-31 13:41:50 +0800

    Initial Commit

---
+++ X2015/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  X2015
+//
+//  Created by Hang Zhang on 12/31/15.
+//  Copyright © 2015 Zhang Hang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
