84dcbffe0e85c0e8bba1381a465c87a9f329170b
ZhangHang <zh199111@gmail.com>
2016-02-27 23:20:52 +0800

    Update build script
    - add submodule update script
    - comments

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -3,4 +3,16 @@
 DIR=$(cd "$(dirname "$0")"; pwd)
 cd $DIR
 cd ..
+
+# fetch third-party frameworks
+#
+# make sure you have cathage installed
+# https://github.com/Carthage/Carthage
 carthage update --platform iOS
+
+# update submodules
+#
+# makr sure you have git installed
+# https://git-scm.com
+git submodule init
+git submodule update --recursive
